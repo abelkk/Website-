e8a90b4a93d47d1c1c4316bd3ba715c9524228c6
Abel kemboi <74068601+abelkk@users.noreply.github.com>
2021-05-03 17:27:08 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,22 @@
+name = input('What is your  name? ')
+print('Hi ' + name)
+color = input('What is your favorite color?')
+print(name + ' likes ' + color)
+birth_year = input('birth year: ')
+print(type(birth_year))
+age = 2021 - int(birth_year)
+print(name + ' was born in ' + (birth_year))
+sport = input("what is your favorite sport? ")
+print(type(sport))
+print(' Good.me too:)')
+team: str = input('what is your favorite team? ')
+print('Good choice:)')
+school = input(name + 'which school do you go to? ')
+print('awesome!')
+print('this is my first ever programm am making')
+opinion = input('what is your opinion? ')
+print('thank you for your feed back ,have a good day')
+
+
+
+
